f22f7eb9d937ccaa33f89b8fc7e28e6cb1b94f6d
Maarten L. Hekkelman <maarten@hekkelman.com>
2022-11-16 08:23:02 +0100

    start examples

---
+++ examples/example.cpp
@@ -0,0 +1,16 @@
+// Example of using libcfp
+
+#include <cfp/cfp.hpp>
+
+int main(int argc, char * const argv[])
+{
+	auto &config = cfp::config::instance();
+
+	config.init(
+		"usage: example [options] file",
+		
+	)
+
+
+	return 0;
+}
