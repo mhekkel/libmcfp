df0d1246ee927fe6952083764a31028a765851bb
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-06-03 14:13:16 +0200

    Update licenses

--- .github/README.md
+++ .github/README.md
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2026 Maarten L. Hekkelman
+
+SPDX-License-Identifier: BSD-2-Clause
+-->
+
 Moved
 =====
 
---
+++ src/charconv-fast_float.cxx
@@ -0,0 +1,7 @@
+// Copyright Maarten L. Hekkelman 2026
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+module;
+
+#include "charconv-fast_float.cpp"
